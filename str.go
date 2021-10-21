474b8257f318dac307837391ad99face9a8d32c5
Vladimir Ivanov <drpodt.ivv@gmail.com>
2021-10-21 18:31:22 +0300

    add comments

--- str.go
+++ str.go
@@ -1,5 +1,7 @@
+//Package implements a set of several functions for working with strings
 package str
 
+//ReverseString reverse a string and returns it
 func ReverseString(s string) string {
 	r := []rune(s)
 	for a, b := 0, len(r)-1; a < b; a, b = a+1, b-1 {
